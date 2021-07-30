cfa1b413a5c14cf7941d45a2ec172aee1816917f
agyago <42577978+agyago@users.noreply.github.com>
2021-07-30 05:50:22 -0600

    initial commit

---
+++ deskclean.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+import datetime
+from pathlib import Path
+import shutil
+
+myfile = Path("/Users/ayago/Desktop/testfolder")
+extentionpaths = { '.png': 'Image', '.jpg': 'Image','.docx': 'Document', '.rpm': 'Package'}
+for file in myfile.iterdir():
+   if file.is_file() and file.name != '.DS_Store':
+       date = file.stat().st_mtime
+       year = datetime.datetime.fromtimestamp(date).strftime("%Y")
+       month = datetime.datetime.fromtimestamp(date).strftime("%B")
+       rootpath= myfile.joinpath(extentionpaths[file.suffix])
+       newpath = rootpath.joinpath(year)
+       subpath = newpath.joinpath(month)
+       if file.suffix in extentionpaths:
+           if not rootpath.exists():
+               rootpath.mkdir()
+           if not newpath.exists():
+               newpath.mkdir()
+           if not subpath.exists():
+               subpath.mkdir()
+           shutil.move(file,subpath)
